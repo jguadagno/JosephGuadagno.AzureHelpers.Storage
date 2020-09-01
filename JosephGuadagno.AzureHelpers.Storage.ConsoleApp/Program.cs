7813fddd7d831d98449b5e8ef1b2c56c4e39860b
jguadagno <jguadagno@hotmail.com>
2020-09-01 16:30:34 -0700

    Added a new Test case

---
+++ JosephGuadagno.AzureHelpers.Storage.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JosephGuadagno.AzureHelpers.Storage.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accountName = "cwjgContacts";
+            var containerName = "contact-images";
+            
+            var blobs = new Blobs(accountName, null, containerName);
+
+            var fileWasDownload = blobs.DownloadToAsync("headshot1.jpg", "c:\\Downloads\\headshot0825-1.jpg").Result;
+            Console.WriteLine($"File was downloaded = {fileWasDownload}");
+            Console.ReadKey();
+        }
+    }
+}
