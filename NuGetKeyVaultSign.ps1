884870361dfd6f3eb87d885feb13c27e7a2b0517
jguadagno <jguadagno@hotmail.com>
2020-11-13 09:26:40 -0700

    Added support for file globs

--- NuGetKeyVaultSign.ps1
+++ NuGetKeyVaultSign.ps1
@@ -1,18 +1,25 @@
 [CmdletBinding()]
 param (
-    [string]$file,
+    [string]$FilePath,
     [string]$AzureKeyVaultUrl = $Env:AzureKeyVaultUrl ,
     [string]$AzureKeyVaultTenantId = $Env:AzureKeyVaultTenantId,
     [string]$AzureKeyVaultClientId = $Env:AzureKeyVaultClientId,
     [string]$AzureKeyVaultClientSecret = $Env:AzureKeyVaultClientSecret,
     [string]$AzureKeyVaultCertificate = "JosephGuadagno-2021"
 )
-.\NuGetKeyVaultSignTool sign $file `
---file-digest "sha256" `
---timestamp-rfc3161 "http://timestamp.digicert.com" `
---timestamp-digest "sha256" `
---azure-key-vault-url $AzureKeyVaultUrl `
---azure-key-vault-tenant-id $AzureKeyVaultTenantId `
---azure-key-vault-client-id $AzureKeyVaultClientId `
---azure-key-vault-client-secret $AzureKeyVaultClientSecret `
---azure-key-vault-certificate $AzureKeyVaultCertificate
+
+$files = Get-ChildItem -Path $FilePath
+
+foreach ($file in $files) {
+    Write-Debug("Signing file: " + $file.FullName)
+
+    .\NuGetKeyVaultSignTool sign $file `
+    --file-digest "sha256" `
+    --timestamp-rfc3161 "http://timestamp.digicert.com" `
+    --timestamp-digest "sha256" `
+    --azure-key-vault-url $AzureKeyVaultUrl `
+    --azure-key-vault-tenant-id $AzureKeyVaultTenantId `
+    --azure-key-vault-client-id $AzureKeyVaultClientId `
+    --azure-key-vault-client-secret $AzureKeyVaultClientSecret `
+    --azure-key-vault-certificate $AzureKeyVaultCertificate
+}
